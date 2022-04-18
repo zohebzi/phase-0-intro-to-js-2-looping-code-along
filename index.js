f51dbbd2e127eef490e8e01f81ef4dd729784ae8
Zoheb Iqbal <zoheb.zafar.iqbal@gmail.com>
2022-04-18 16:38:03 -0400

    commit tree 1a47f9f69d8c649e5c477607895f14ba97b250ab

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards (names, event) {
+    let newArr = [];
+    for (let i = 0; i < names. length; i++) {
+        let message = (`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+        newArr.push(message);
+    }
+
+    return newArr; }
+
+    function countDown(int) {
+        while (int>=0) {
+            console.log (int);
+            int--
+        }
+    }
